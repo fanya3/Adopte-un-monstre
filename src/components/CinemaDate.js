40d914a4421f0fd6016dbbf5c942f637d2ed9855
Fanny <fanny.bouffier@gmail.com>
2019-10-30 23:18:08 +0100

    creation of API movie request

---
+++ src/components/CinemaDate.js
@@ -0,0 +1,38 @@
+import React from "react"
+import './CinemaDate.css';
+import axios from 'axios'
+
+
+
+
+class CinemaDate extends React.Component{
+    state={
+        movie : [], 
+    }
+
+    getMovies = () => {
+        const randomId=Math.floor(Math.random() * (81))
+        axios.get(`https://hackathon-wild-hackoween.herokuapp.com/movies`)
+        .then(response => response.data)
+        .then(data => {console.log("test Api", data) || 
+        this.setState({movie: data.movies[randomId]})
+    })
+         }
+
+         componentDidMount(){
+            this.getMovies()
+         }
+
+    render(){
+        return(
+            
+            <div className="cinema-ticket"> 
+            <h3>{this.state.movie.title}</h3>
+            <img className="movie" src={this.state.movie.posterUrl} alt={this.state.movie.title}></img>
+            </div>
+
+                
+        )
+    }
+}
+export default CinemaDate
