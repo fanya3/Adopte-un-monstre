7d2ffdc6f1a284f65bb2dbb8123475698789b914
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2019-10-30 15:52:52 +0100

    Base react

---
+++ src/component/DatingFeature.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+class DatingFeature extends React.Component{
+    render(){
+        return(
+            <div>
+                <p>coucou</p>
+            </div>
+        )
+    }
+}
+export default DatingFeature
