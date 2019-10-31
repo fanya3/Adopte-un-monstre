dc4aa0818a083c2412f5a87a48c8b47d0aa192de
Morgan-Leblanc <morgan.leblanc91@gmail.com>
2019-10-31 12:48:24 +0100

    modif player audio

--- src/components/DatingFeature.js
+++ src/components/DatingFeature.js
@@ -1,37 +1,39 @@
 import React from "react"
 import './DatingFeature.css';
 import video from '../assets/Video/videoFirst.mp4'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import audio from '../assets/Audio/adopt.mp3'
 
-class DatingFeature extends React.Component{
-    render(){
-        return(
+class DatingFeature extends React.Component {
+    render() {
+        return (
             <div>
+                <audio autoPlay loop >
+                    {console.log('joué')}
+                    <source src={audio} type="audio/mp3"></source>
+                </audio>
                 <video autoPlay loop muted >
                     <source src={video} type="video/mp4"></source>
                 </video>
-                <audio autoPlay loop>
-                    <source src={audio} typ="audio/mp3"></source>
-                </audio>
+                
                 <div className="welcome">
-                <h1 className="text middle" data-text="adopt a monster">ADOPT A MONSTER</h1>
+                    <h1 className="text middle" data-text="adopt a monster">ADOPT A MONSTER</h1>
                     <p>Ready to try ?</p>
                     <div className="choice">
-                        <Link 
-                        to='/Elevator' 
-                        className="button-welcome"
-                        type='button'>
-                        YES
+                        <Link
+                            to='/Elevator'
+                            className="button-welcome"
+                            type='button'>
+                            YES
                     </Link>
-                    <Link
-                        to='/ifNoScreen'
-                        className="button-welcome"
-                        type='button'>
-                        NO
+                        <Link
+                            to='/ifNoScreen'
+                            className="button-welcome"
+                            type='button'>
+                            NO
                     </Link>
                     </div>
-                    
+
                 </div>
             </div>
         )
